5fb464b21e42af02db328744704476bdf816e62e
aijj2511-0707 <aijj2511@gmail.com>
2026-07-08 17:12:08 +0900

    시간개선

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const serviceAccount = {
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
 
   private_key:
-  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+(process.env.FIREBASE_PRIVATE_KEY || "")
+.replace(/\\n/g,"\n"),
 
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
 
